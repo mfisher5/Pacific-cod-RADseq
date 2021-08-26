71775ff6784a76cb117cbbe202462a0eb1d88ce8
mfisher5 <mfisher5@uw.edu>
2021-08-25 18:03:09 -0700

    migrate input file prep

---
+++ scripts/Migrate/subset_populations_fasta.py
@@ -0,0 +1,7 @@
+################# Subset Stacks populations FASTA file #################
+# 
+# This python script will subset the fasta file output by populations
+#   to include only the loci specified in a text file.
+# 2021-08-25 M. Fisher
+#
+########################################################################
